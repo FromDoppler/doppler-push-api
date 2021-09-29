3189b27b4e097d0af14c9a870d200006da0dd716
ms-darianbenito <dbenito@makingsense.com>
2021-09-29 15:14:37 -0300

    feat: remove sent message handing

--- Doppler.Push.Api/Controllers/MessageController.cs
+++ Doppler.Push.Api/Controllers/MessageController.cs
@@ -1,11 +1,8 @@
 using Doppler.Push.Api.Contract;
 using Doppler.Push.Api.DopplerSecurity;
 using Doppler.Push.Api.Services;
-using Doppler.Push.Api.Services.FirebaseSentMessagesHandling;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Threading.Tasks;
 
 namespace Doppler.Push.Api.Controllers
@@ -15,32 +12,17 @@ namespace Doppler.Push.Api.Controllers
     [Route("[controller]")]
     public class MessageController : ControllerBase
     {
-        private readonly IFirebaseCloudMessageService _firebaseCloudMessageService;
-        private readonly IFirebaseSentMessagesHandler _firebaseSentMessagesHandler;
-        private readonly ILogger<MessageController> _logger;
+        private IFirebaseCloudMessageService _firebaseCloudMessageService;
 
-        public MessageController(IFirebaseCloudMessageService firebaseCloudMessageService,
-                                IFirebaseSentMessagesHandler firebaseSentMessagesHandler,
-                                ILogger<MessageController> logger)
+        public MessageController(IFirebaseCloudMessageService firebaseCloudMessageService)
         {
             _firebaseCloudMessageService = firebaseCloudMessageService;
-            _firebaseSentMessagesHandler = firebaseSentMessagesHandler;
-            _logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> MessageSend(FirebaseMessageSendRequest messageSend)
         {
             var response = await _firebaseCloudMessageService.SendMulticast(messageSend);
-
-            try
-            {
-                await _firebaseSentMessagesHandler.HandleSentMessagesAsync(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, "Error handling sent messages");
-            }
 
             return Ok(response);
         }
--- Doppler.Push.Api/Services/FirebaseSentMessagesHandling/IPushContactApiTokenGetter.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Doppler.Push.Api.Services.FirebaseSentMessagesHandling
-{
-    public interface IPushContactApiTokenGetter
-    {
-        Task<string> GetTokenAsync();
-    }
-}
